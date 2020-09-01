ba627f21edd73a7ec10fecdd6a018a1536cb49f7
jenspetur94 <jenspeturtrondarson@gmail.com>
2020-09-01 16:42:26 +0200

    initial project structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>
+          Hello world !
+        </h1>
+      </header>
+      <body>
+        <Button>
+          This is a button !
+        </Button>
+      </body>
+    </div>
+  );
+}
+
+export default App;
